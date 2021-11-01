f4b8095f17003ee2a19b072ad700089e42932bfa
imran <imran.ansari2031999@gmail.com>
2021-11-01 01:40:32 -0700

    hello

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 # Create your views here.
 
@@ -7,11 +9,42 @@ def index(request):
     return render(request, 'index.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def lgin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            uname = form.cleaned_data.get('username')
+            pas = form.cleaned_data.get('password')
+            user = authenticate(username=uname, password=pas)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+        else:
+
+            form = AuthenticationForm()
+            context = {'form': form}
+            return render(request, 'login.html', context)
 
 
 def signup(request):
-    form= UserCreationForm()
-    context={'form':form}
-    return render(request, 'signup.html',context)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            print('form is valid')
+            user = form.save()
+            if user is not None:
+                return redirect('index')
+        else:
+            print('invalid')
+            return render(request, 'signup.html', context)
+
+
+
+
+    else:
+        form = UserCreationForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'signup.html', context)
